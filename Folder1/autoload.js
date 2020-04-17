fd0a763e55e89b21a44adc9661822fc671a8758b
Knight-of-night <61961828+Knight-of-night@users.noreply.github.com>
2020-04-17 23:25:16 +0800

    Update autoload.js

--- Folder1/autoload.js
+++ Folder1/autoload.js
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://cdn.jsdelivr.net/gh/Knight-of-night/Knight-of-night.github.io@latest/Folder2/",
+			apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
