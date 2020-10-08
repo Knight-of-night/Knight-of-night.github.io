50568491fbff3b9ddda001099ffda59e71989abc
Knight-of-night <724869286@qq.com>
2020-10-08 23:16:24 +0800

    update

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-  const data = {};
-  const $ = window.document.querySelector.bind(window.document);
-  fontSize = document.body.clientWidth / 40;
-  document.getElementsByTagName('html')[0].style['font-size'] = fontSize + 'px';
-};
